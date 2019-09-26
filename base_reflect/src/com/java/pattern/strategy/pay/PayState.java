48b55b31126e2152c1ad8daeb77fbc574d42d7fe
foreverhahaha <71523358@qq.com>
2019-09-26 18:24:58 +0800

    prototype strategy

---
+++ base_reflect/src/com/java/pattern/strategy/pay/PayState.java
@@ -0,0 +1,23 @@
+package com.java.pattern.strategy.pay;
+
+/**
+ * @author: lsm
+ * @description:
+ * @create: 2019-09-26 18:03
+ */
+public class PayState {
+
+    private int code;
+    private Object data;
+    private String msg;
+
+    public PayState(int code, String msg,Object data) {
+        this.code = code;
+        this.data = data;
+        this.msg = msg;
+    }
+
+    public String toString(){
+        return ("支付状态：[" + code + "]," + msg + ",交易详情：" + data);
+    }
+}
