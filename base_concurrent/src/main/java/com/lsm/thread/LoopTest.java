d14267e257e5584dfbe11352981606b105b9e404
liusanmu <13053047567@163.com>
2020-02-01 00:31:09 +0800

    目录-2020/2/1

---
+++ base_concurrent/src/main/java/com/lsm/thread/LoopTest.java
@@ -0,0 +1,23 @@
+package com.lsm.thread;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**  
+ * @Description: LoopTest.java
+ * @author: lsm
+ * @date: 2019年1月9日 上午10:15:51   
+ */
+public class LoopTest {
+	
+	/**
+	 * 子线程循环 10  次 ， 接着主线程循环 100 ， 接着又回到子线程循环 10  次 ， 接着再回到主线程又循环 100 ，如此循环 50 
+	 * @throws Exception
+	 */
+	@Test
+	public void test1() throws Exception {
+		
+	}
+
+}
---
+++ base_concurrent/src/main/java/com/lsm/thread/demo1/SellTicketDemo.java
@@ -0,0 +1,46 @@
+package com.lsm.thread.demo1;
+
+
+/**  
+ * @Description: SellTicketDemo.java
+ * @author: liusen
+ * @date:   2019年4月26日 上午10:30:14   
+ */
+public  class SellTicketDemo {
+
+	public static void main(String[] args) {
+		Ticket ticket = new Ticket();
+		new Thread(ticket,"窗口一").start();
+		new Thread(ticket,"窗口二").start();
+		new Thread(ticket,"窗口三").start();
+		new Thread(ticket,"窗口四").start();
+	}
+}
+
+
+class Ticket implements Runnable{
+	
+	private int num = 10;
+	Object lock = new Object();
+	
+	@Override
+	public void run() {
+		while(true){
+			synchronized (Ticket.class) {
+			if (num>0) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			
+					System.out.println(Thread.currentThread().getName() + "正在卖第" + num--);
+				}
+				
+			}
+			
+		}
+		
+	}
+
+}
